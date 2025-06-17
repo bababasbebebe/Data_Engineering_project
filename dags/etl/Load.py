b1766c7bb086660f9b1a746ebc5ace1da2df04d8
Alexandr Vyachin <81687162+bababasbebebe@users.noreply.github.com>
2025-06-17 18:39:39 +0300

    Update Load.py

--- dags/etl/Load.py
+++ dags/etl/Load.py
@@ -2,6 +2,11 @@ import pandas as pd
 from sklearn.datasets import load_breast_cancer
 import matplotlib.pyplot as plt
 import logging
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+RESULTS_DIR = os.path.join(BASE_DIR, 'results')
+os.makedirs(RESULTS_DIR, exist_ok=True)
 
 logger = logging.getLogger(__name__)
 
@@ -13,16 +18,16 @@ def load_data():
 
         # Сохранение info
         pd.DataFrame({"Column": features.columns, "Non-Null Count": len(features) - features.isnull().sum().values,
-                      "Dtype": features.dtypes.values}).to_csv("features_info.csv")
+                      "Dtype": features.dtypes.values}).to_csv(os.path.join(RESULTS_DIR, 'features_info.csv'), index=False)
 
         # Сохранение describe
-        features.describe().to_csv('features_describe.csv')
+        features.describe().to_csv(os.path.join(RESULTS_DIR, 'features_describe.csv'), index=False)
 
         target = pd.DataFrame(load_breast_cancer()['target'], columns=['target'])
         target.hist()
 
         # Сохранение распределения target
-        plt.savefig('figure.pdf')
+        plt.savefig(os.path.join(RESULTS_DIR, 'features.csv'), index=False)
 
         logger.info(f"Данных успешно загружены и info, describe и распределение target сохранено")
 
